517b52c8a1fd83f6a28ede6bc24d82f9452370da
mohammadnedaei <nedaei2002@gmail.com>
2021-12-31 17:52:56 +0330

    Improve StickyBanner

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ function App() {
                 bannerTitle={"Big news!"}
                 bannerDescription={"Introducing Android version of app."}
                 bannerButtonText={"Learn more"}
+                bannerFixedPlace={"top"}
                 bannerButtonUrl={"https://gitlab.com"}
             />
+
             <h1>Hi</h1>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <h1>Hi</h1>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <h1>Hi</h1>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </div>
     );
 }
--- src/components/StickyBanner.jsx
+++ src/components/StickyBanner.jsx
@@ -1,13 +1,20 @@
 import {SpeakerphoneIcon, XIcon} from '@heroicons/react/outline';
-import {useState} from "react";
+import {useMemo, useState} from "react";
 
 export default function StickyBanner(props) {
     const [show, setShow] = useState(true);
+    const bannerPlace = useMemo(() => {
+        return props.bannerFixedPlace == 'bottom' ? {
+            bottom: 0
+        } : {
+            top:0
+        }
+    }, [])
     if (window.localStorage.getItem('seen_banner')) {
         return <></>
     }
         return (show ?
-            <div className="bg-indigo-600">
+            <div style={{position:'fixed', width: '100%', ...props.bannerStyle, ...bannerPlace}} className={`bg-indigo-600 ${props.bannerBackground}`}>
                 <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between flex-wrap">
                         <div className="w-0 flex-1 flex items-center">
@@ -24,7 +31,8 @@ export default function StickyBanner(props) {
                             <a
                                 onClick={props.bannerButtonOnClick}
                                 href={props.bannerButtonUrl}
-                                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+                                style={props.bannerButtonStyle}
+                                className={`flex items-center justify-center py-2 px-3 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50 ${props.bannerButtonClass}`}
                             >
                                 {props.bannerButtonText}
                             </a>
