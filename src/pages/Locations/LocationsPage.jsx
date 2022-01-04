ed70689d3e11cc23eb5e7f329153552070aa42c3
mohammadnedaei <nedaei2002@gmail.com>
2022-01-04 22:13:13 +0330

    handle request foursquare with axios

--- src/pages/Locations/LocationsPage.jsx
+++ src/pages/Locations/LocationsPage.jsx
@@ -1,4 +1,17 @@
+import axios from "axios";
+
 const LocationsPage = () => {
+    axios.get('https://api.foursquare.com/v3/places/search?ll=31%2C31', {
+        headers: {
+            Authorization: 'fsq3Pf7uqayKsWP6RCLPbEhnAo/WE5mbmhzJum6RNbei4nI='
+        }
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     return(
         <h1>Hi</h1>
     )
