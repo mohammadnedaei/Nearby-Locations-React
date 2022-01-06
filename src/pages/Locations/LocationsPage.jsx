9605568505e0dc3b356721b7b5dbacbbb1a83bd1
mohammadnedaei <nedaei2002@gmail.com>
2022-01-06 16:13:42 +0330

    save data in state

--- src/pages/Locations/LocationsPage.jsx
+++ src/pages/Locations/LocationsPage.jsx
@@ -1,17 +1,29 @@
 import axios from "axios";
+import {useEffect, useState} from "react";
 
 const LocationsPage = () => {
-    axios.get('https://api.foursquare.com/v3/places/search?ll=31%2C31', {
-        headers: {
-            Authorization: 'fsq3Pf7uqayKsWP6RCLPbEhnAo/WE5mbmhzJum6RNbei4nI='
-        }
-    })
-        .then(function (response) {
-            console.log(response);
+    const [locationData , setLocationData] = useState({});
+    useEffect(() => {
+        axios.get('https://api.foursquare.com/v3/places/search?ll=31%2C31', {
+            headers: {
+                Authorization: 'fsq3Pf7uqayKsWP6RCLPbEhnAo/WE5mbmhzJum6RNbei4nI='
+            }
         })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .then(function (response) {
+                console.log(response);
+                if (response.status === 200) {
+                    setLocationData(response);
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },[])
+
+    useEffect(() => {
+        alert(JSON.stringify(locationData));
+    },[locationData])
+
     return(
         <h1>Hi</h1>
     )
