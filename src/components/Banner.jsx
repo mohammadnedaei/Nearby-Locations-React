d33f37cc57b32a721d1effd1d5a861f88d725e28
mohammadnedaei <nedaei2002@gmail.com>
2022-01-08 13:37:23 +0330

    handle Banner responsiveness

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -16,16 +16,16 @@ export default function Banner(props) {
     }
         return (show ?
             <div style={{zIndex: 99, ...props.bannerStyle, ...bannerPlace}} className={`bg-indigo-600 ${props.bannerBackground}`}>
-                <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto py-5 px-5 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between flex-wrap">
                         <div className="w-0 flex-1 flex items-center">
             <span className="flex p-2 rounded-lg bg-indigo-800">
-              <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true"/>
+              <SpeakerphoneIcon className="h-7 w-7 text-white" aria-hidden="true"/>
             </span>
-                            <p className="ml-3 font-medium text-white truncate">
+                            <p className="ml-3 font-medium text-lg text-white truncate">
                                 <span className="md:hidden">{props.bannerDescription}</span>
                                 <span
-                                    className="hidden md:inline">{props.bannerTitle} {props.bannerDescription}</span>
+                                    className="hidden md:inline"><span className="font-bold">{props.bannerTitle}</span> {props.bannerDescription}</span>
                             </p>
                         </div>
                         <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
@@ -33,7 +33,7 @@ export default function Banner(props) {
                                 onClick={props.bannerButtonOnClick}
                                 href={props.bannerButtonUrl}
                                 style={props.bannerButtonStyle}
-                                className={`flex items-center justify-center py-2 px-3 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50 ${props.bannerButtonClass}`}
+                                className={`flex items-center justify-center py-2 px-3 border border-transparent rounded-md shadow-sm text-md font-medium text-indigo-600 bg-white hover:bg-indigo-50 ${props.bannerButtonClass}`}
                             >
                                 {props.bannerButtonText}
                             </a>
