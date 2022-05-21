96facded0b0eb72caa2ae44c72f8c3637b86958f
mohammadnedaei <nedaei2002@gmail.com>
2022-05-21 20:29:07 +0430

    added temporary static skeletons

--- src/pages/Locations/LocationDetails.jsx
+++ src/pages/Locations/LocationDetails.jsx
@@ -7,10 +7,12 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Gradient from 'rgt'
 import 'swiper/swiper.scss';
 import './locationDetailsStyles.scss';
+import {Skeleton} from "@mui/material";
 const LocationDetails = () => {
     const randomNumber = useMemo(() => {
         return Math.floor(Math.random() * 11);
     },[])
+    //TODO: properly skeleton for photos grid !important
     const gradientColors = useMemo(() => {
         return ['#004d4b',
             '#007CF0',
@@ -25,7 +27,10 @@ const LocationDetails = () => {
             '#6441A5',
             '#42106e']
     }, [])
-
+    const sampleImage = [
+        'https://via.placeholder.com/1080x900.png?text=Loading...',
+        'https://via.placeholder.com/1080x700.png?text=No image is provided for this location :('
+    ]
     const {state} = useLocation();
     const { location, locationName, locationId } = state;
     const {requestCallback, locationPhotos} = useLocationPhotos()
@@ -36,12 +41,6 @@ const LocationDetails = () => {
         },[locationId])
 console.log(randomNumber)
     console.log(locationPhotos)
-
-    const samplePhotos = [
-    'https://via.placeholder.com/1080x900',
-
-
-]
 return (
   <div style={{display:'flex', alignItems:'center', flexDirection:'column'}} className="page">
       <div style={{display:'flex' , flexDirection:'row'}}>
@@ -72,8 +71,31 @@ return (
 
           </div>
       </div>
-      {locationPhotos !== null ?
+      {locationPhotos == null ?
+          <div>
+              <div style={{display:'flex', justifyContent:'center'}}>
+                  <div className="w-4_10 lg-w-1_2 md-w-1 mr-5">
+                      <img className="mt-10 start-rounded md-end-rounded" alt = "1080x900" src={sampleImage[0]}/>
+                  </div>
+                  <div className="alternative-photos lg-w-1_2 w-4_10 md-visible">
+                      <div className="w-1_2 lg-w-1 flex">
+                          <img className="mt-10 mr-5 lg-end-rounded" alt = "1080x900" src={sampleImage[0]} />
+                          <img className="lg-visible mt-10 top-end-rounded" alt = "1080x900" src={sampleImage[0]} />
+                      </div>
+                      <div className="lg-visible w-1_2 flex">
+                          <img style={{marginRight: '20px'}} className="mt-10" alt = "1080x900" src={sampleImage[0]} />
+                          <img className="mt-10 bottom-end-rounded" alt = "1080x900" src={sampleImage[0]} />
+                      </div>
+                  </div>
+              </div>
+          </div>
+          :
       <div>
+          {locationPhotos.length === 0 ?
+              <div>
+                  <img className="mt-10 rounded" alt ="1080x700" src={sampleImage[1]}/>
+              </div>
+              : null }
           {locationPhotos.length === 1 ?
              <div>
                  <img className="mt-10 rounded" alt ="1080x700" src={locationPhotos[0].prefix + "1080x700" + locationPhotos[0].suffix}/>
@@ -82,31 +104,31 @@ return (
           {locationPhotos.length === 2 ?
               <div>
                       <div style={{display:'flex', justifyContent: 'center'}}>
-                          <img  className="mt-10 main-photo w-4_10 mr-5 start-rounded" alt = "1080x900" src={samplePhotos[0]}/>
-                          <img style={{width: "40%"}} className="mt-10 lg-visible end-rounded" alt = "1080x900" src={samplePhotos[0]}/>
+                          <img  className="mt-10 main-photo w-4_10 mr-5 start-rounded" alt = "1080x900" src={locationPhotos[0].prefix + "1080x900" + locationPhotos[0].suffix}/>
+                          <img style={{width: "40%"}} className="mt-10 lg-visible end-rounded" alt = "1080x900" src={locationPhotos[1].prefix + "1080x900" + locationPhotos[1].suffix}/>
                       </div>
               </div>
               : null }
           {locationPhotos.length === 3 ?
               <div style={{display:'flex', justifyContent: 'center'}}>
                   <div className="w-6_10 lg-w-1_2 md-w-1 mr-5">
-                      <img className="mt-10 start-rounded md-end-rounded" alt = "1080x900" src={samplePhotos[0]}/>
+                      <img className="mt-10 start-rounded md-end-rounded" alt = "1080x900" src={locationPhotos[0].prefix + "1080x600" + locationPhotos[0].suffix}/>
                   </div>
                   <div className="alternative-photos lg-w-1_2 w-3_10 md-visible">
-                      <img className="mt-10 lg-visible top-end-rounded" alt = "1080x900" src={samplePhotos[1]}/>
-                      <img className="mt-10 bottom-end-rounded lg-top-end-rounded" alt = "1080x900" src={samplePhotos[2]}/>
+                      <img className="mt-10 lg-visible top-end-rounded" alt = "1080x900" src={locationPhotos[1].prefix + "1080x600" + locationPhotos[1].suffix}/>
+                      <img className="mt-10 bottom-end-rounded lg-top-end-rounded" alt = "1080x900" src={locationPhotos[2].prefix + "1080x600" + locationPhotos[2].suffix}/>
                   </div>
               </div>
               : null }
           {locationPhotos.length === 4 ?
               <div style={{display:'flex', justifyContent: 'center'}}>
                   <div className="w-6_10 lg-w-1_2 md-w-1 mr-5">
-                      <img className="mt-10 start-rounded md-end-rounded" alt = "1080x900" src={samplePhotos[0]}/>
+                      <img className="mt-10 start-rounded md-end-rounded" alt = "1080x900" src={locationPhotos[0].prefix + "1080x700" + locationPhotos[0].suffix}/>
                   </div>
                   <div className="alternative-photos lg-w-1_2 w-1_6 md-visible">
-                      <img className="mt-10 lg-visible top-end-rounded" alt = "1080x900" src={samplePhotos[0]}/>
-                      <img className="mt-10 lg-visible" alt = "1080x900" src={samplePhotos[0]}/>
-                      <img className="mt-10 bottom-end-rounded lg-top-end-rounded" alt = "1080x900" src={samplePhotos[0]}/>
+                      <img className="mt-10 lg-visible top-end-rounded" alt = "1080x900" src={locationPhotos[1].prefix + "1080x700" + locationPhotos[1].suffix}/>
+                      <img className="mt-10 lg-visible" alt = "1080x900" src={locationPhotos[2].prefix + "1080x700" + locationPhotos[2].suffix}/>
+                      <img className="mt-10 bottom-end-rounded lg-top-end-rounded" alt = "1080x900" src={locationPhotos[3].prefix + "1080x700" + locationPhotos[3].suffix}/>
                   </div>
               </div>
               : null }
@@ -128,7 +150,7 @@ return (
                 </div>
             </div>
         </div> : null}
-      </div> :null}
+      </div>}
   </div>
 )
 }
