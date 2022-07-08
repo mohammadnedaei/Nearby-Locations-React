8981579f7c5d3b0c4222aec75bb20f66d05db95b
mohammadnedaei <nedaei2002@gmail.com>
2022-07-08 18:12:41 +0430

    Refactor

--- src/pages/Locations/LocationDetails/components/LocationTitle.jsx
+++ src/pages/Locations/LocationDetails/components/LocationTitle.jsx
@@ -1,22 +1,25 @@
 import Gradient from "rgt";
-import textGradients from "../../../../configs/textGradients";
+import TextGradientColors from "../../../../configs/TextGradientColors";
 import * as React from "react";
 import {useMemo} from "react";
 
 const LocationTitle = (props) => {
     const randomNumber = useMemo(() => {
         return Math.floor(Math.random() * 11);
-    },[])
+    }, [])
     return (
-        <div style={{display:'flex' , flexDirection:'row'}}>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
             <div>
-                <h1 style={{marginTop: 25, marginBottom: 5}} className="location-title text-2xl font font-extrabold tracking-tight text-gray-900">
+                <h1 style={{marginTop: 25, marginBottom: 5}}
+                    className="location-title text-2xl font font-extrabold tracking-tight text-gray-900">
                     {randomNumber % 2 === 0 ?
-                        <Gradient dir="left-to-right" from={textGradients[randomNumber]} to={textGradients[randomNumber+1]}>
+                        <Gradient dir="left-to-right" from={TextGradientColors[randomNumber]}
+                                  to={TextGradientColors[randomNumber + 1]}>
                             {props.title}
                         </Gradient>
                         :
-                        <Gradient dir="left-to-right" from={textGradients[randomNumber]} to={textGradients[randomNumber-1]}>
+                        <Gradient dir="left-to-right" from={TextGradientColors[randomNumber]}
+                                  to={TextGradientColors[randomNumber - 1]}>
                             {props.title}
                         </Gradient>
                     }
@@ -24,11 +27,13 @@ const LocationTitle = (props) => {
                 </h1>
                 <span>
                   {randomNumber % 2 === 0 ?
-                      <Gradient dir="right-to-left" from={textGradients[randomNumber]} to={textGradients[randomNumber+1]}>
+                      <Gradient dir="right-to-left" from={TextGradientColors[randomNumber]}
+                                to={TextGradientColors[randomNumber + 1]}>
                           {props.description}
                       </Gradient>
                       :
-                      <Gradient dir="right-to-left" from={textGradients[randomNumber]} to={textGradients[randomNumber-1]}>
+                      <Gradient dir="right-to-left" from={TextGradientColors[randomNumber]}
+                                to={TextGradientColors[randomNumber - 1]}>
                           {props.description}
                       </Gradient>
                   }
