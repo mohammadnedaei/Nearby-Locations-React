f819644fc466a19950b60224f2b728db87bdaad9
mohammadnedaei <nedaei2002@gmail.com>
2022-07-08 18:11:42 +0430

    Refactor: Added env to hooks

--- src/hook/useLocationPhotos.jsx
+++ src/hook/useLocationPhotos.jsx
@@ -4,24 +4,22 @@ import axios from "axios";
 const useLocationPhotos = () => {
     const [locationPhotos, setlocationPhotos] = useState(null);
     const requestCallback = useCallback((id) => {
-        console.log(id)
-            axios.get('https://api.foursquare.com/v3/places/' + id + '/photos', {
-                headers: {
-                    //TODO: rating api (details)
-                    //TODO: Add env for both cors (add localhost on fsq)
-                    Authorization: 'fsq3450KUYbLF7rdOpFn3lmqe0i+Vp+5vtCJQtQ0ztb8pUk=',
-                    'Access-Control-Allow-Origin': '*'
+        axios.get('https://api.foursquare.com/v3/places/' + id + '/photos', {
+            headers: {
+                //TODO: rating api (details)
+                //TODO: Add env for both cors (add localhost on fsq)
+                Authorization: process.env.REACT_APP_FSQ_KEY,
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+            .then(function (response) {
+                if (response.status === 200) {
+                    setlocationPhotos(response.data);
                 }
             })
-                .then(function (response) {
-                    if (response.status === 200) {
-                        console.log("photos:" + response.data);
-                        setlocationPhotos(response.data);
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+            .catch(function (error) {
+                console.log(error);
+            });
     }, [])
 
 
--- src/hook/useLocationRequest.jsx
+++ src/hook/useLocationRequest.jsx
@@ -6,22 +6,18 @@ function useLocationRequest(pos) {
     const [call, setCall] = useState(true);
     const requestCallback = useCallback((pos) => {
         let coordinate = pos.coords;
-        console.log(pos)
         let lat = coordinate.latitude;
         let long = coordinate.longitude;
         const geoCode = 'll=' + lat + '%2C' + long;
-        console.log(geoCode);
         if (call) {
-            axios.get('https://api.foursquare.com/v3/places/search?' + geoCode + '&limit=50', {
+            axios.get(process.env.REACT_APP_FSQ_PLACES_URL + geoCode + process.env.REACT_APP_FSQ_PLACES_LIMIT, {
                 headers: {
-                    //TODO: Add env for both cors (add localhost on fsq)
-                    Authorization: 'fsq3450KUYbLF7rdOpFn3lmqe0i+Vp+5vtCJQtQ0ztb8pUk=',
+                    Authorization: process.env.REACT_APP_FSQ_KEY,
                     'Access-Control-Allow-Origin': '*'
                 }
             })
                 .then(function (response) {
                     if (response.status === 200) {
-                        console.log(response);
                         setCall(false);
                         setLocationData(response.data.results);
                     }
@@ -31,7 +27,7 @@ function useLocationRequest(pos) {
                     setCall(false);
                 });
         }
-    }, [locationData])
+    }, [call])
 
     return {locationData, requestCallback};
 }
